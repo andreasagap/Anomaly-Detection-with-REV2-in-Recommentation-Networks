dd162fc4476f72385e0757b63af901213613a22a
Andreas <andreasagap@gmail.com>
2021-01-06 01:24:36 +0200

    Experiments

--- rev2.py
+++ rev2.py
@@ -6,13 +6,27 @@ import matplotlib.pyplot as plt
 def runRev2():
     a1, a2, b1, b2, g1, g2, g3 = 1, 1, 1, 1, 1, 1, 1
 
-    #df = pd.read_csv('ratings_musical_Amazon.csv', names=["user", "product", "rating"])
+    df = pd.read_csv('ratings_musical_Amazon.csv', names=["user", "product", "rating"])
 
-    df = pd.read_csv('amazonWithoutUsersOneTime.csv', names=["user", "product", "rating"])
+    # df = pd.read_csv('amazonWithoutUsersOneTime.csv', names=["user", "product", "rating"])
     # print("Network has %d nodes" % (len(df["user"].index)))
-    # df = df[df.groupby('user').user.transform(len) > 14]
-    # df.to_csv(r'amazonWithoutUsersOneTime.csv', index=False, header=False)
-    # print("Network has %d nodes" % (len(df["user"].index)))
+    active = True
+    numberUser = 0
+    numberProduct = 0
+    while active:
+        df = df[df.groupby('product').product.transform(len) > 9]
+        # df = df[df.groupby('product').product.transform(len) < 6]
+        df = df[df.groupby('user').user.transform(len) > 3]
+        print("Network has %d nodes and %d products" % (numberUser, numberProduct))
+
+        if numberUser != len(df["user"].unique()) or numberProduct != len(df["product"].unique()):
+            numberUser = len(df["user"].unique())
+            numberProduct = len(df["product"].unique())
+        else:
+            active = False
+
+    df.to_csv(r'amazonWithoutUsersOneTime.csv', index=False, header=False)
+    print("Network has %d nodes" % (len(df["user"].index)))
     df["Reliability"] = 0
 
     # convert {1,2,3,4,5} ratings to {-1,-0.5,0, 0.5, 1}
@@ -22,13 +36,13 @@ def runRev2():
     usersDF["Fairness"] = 0
 
     # get only 10 first users
-    #usersDF = usersDF[:5000]
+    # usersDF = usersDF[:5000]
     #
     # # list of user ids
-    #list_of_users = usersDF["User"].tolist()
+    # list_of_users = usersDF["User"].tolist()
     #
     # get ratings only for these specific group of users
-    #df = df[df['user'].isin(list_of_users)]
+    # df = df[df['user'].isin(list_of_users)]
 
     # create products dataframe
     productsDF = pd.DataFrame(df["product"].unique(), columns=["Product"])
@@ -68,7 +82,6 @@ def runRev2():
             dp += abs(productsDF.loc[index, 'Goodness'] - gp)
             productsDF.loc[index, 'Goodness'] = gp
 
-
         # FAIRNESS
         for index, row in usersDF.iterrows():
 
@@ -101,16 +114,17 @@ def runRev2():
             df.loc[index, 'Reliability'] = r_up
 
         iter += 1
-        print("Du %f Dp %f Dr %f" % (du,dp,dr))
+        print("Du %f Dp %f Dr %f" % (du, dp, dr))
         if du < 0.01 and dp < 0.01 and dr < 0.01:
             break
 
     usersDF = usersDF.sort_values(by=["Fairness"], ascending=True)
-    productsDF=productsDF.sort_values(by=["Goodness"], ascending=False)
-    df=df.sort_values(by=["Reliability"], ascending=False)
-    productsDF.to_csv (r'products.csv', index = False, header=True)
-    usersDF.to_csv (r'users.csv', index = False, header=True)
-    df.to_csv (r'ratings.csv', index = False, header=True)
+    productsDF = productsDF.sort_values(by=["Goodness"], ascending=False)
+    df = df.sort_values(by=["Reliability"], ascending=False)
+    productsDF.to_csv(r'products.csv', index=False, header=True)
+    usersDF.to_csv(r'users.csv', index=False, header=True)
+    df.to_csv(r'ratings.csv', index=False, header=True)
+
 
 def draw_graph(G):
     plt.figure()
@@ -127,14 +141,14 @@ def draw_graph(G):
                                                                                                          data=True))])
 
     nx.draw_networkx_edges(G, pos, edgelist=edges, label=weights)
-
+    nx.write_gexf(G, "amazon4_10.gexf")
     # nx.draw_networkx(G, pos,with_labels=False, edges=edges,width=weights)
     plt.show()
 
 
 def createGraph():
-    df = pd.read_csv('ratings_musical_Amazon.csv', names=["user", "product", "rating"])
-    df = df.head(20)
+    df = pd.read_csv('amazonWithoutUsersOneTime.csv', names=["user", "product", "rating"])
+
     G = nx.Graph()
     users = df["user"].unique()
     products = df["product"].unique()
@@ -149,4 +163,5 @@ def createGraph():
 
 
 if __name__ == '__main__':
-    runRev2()
+    #runRev2()
+    createGraph()
